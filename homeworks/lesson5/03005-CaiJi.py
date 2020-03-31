b88fceba36d26e97b8307e500c5d1b796cb810a8
Cai Ji <jvfsdfkfwbdsbwsdj@foxmail.com>
2020-03-31 15:45:05 +0800

    push

--- homeworks/lesson5/03005-CaiJi.py
+++ homeworks/lesson5/03005-CaiJi.py
@@ -3,6 +3,45 @@
 
 
 
+import nuke
+import subprocess
+
+
+def Render_Write(nk_file):
+    nuke.scriptReadFile(nk_file)
+
+    write_list = nuke.allNodes("Write")
+
+    if not write_list:
+        print "This file has no Write node"
+        return
+
+    write_name_list = [i.name() for i in write_list]
+
+    print
+    print "This file contains the following write nodes:"
+    print write_name_list
+    print "Enter the name of the write node to be rendered:"
+    print
+    in_write_name = raw_input()
+    if not in_write_name in write_name_list:
+        print "Without this write node"
+        return
+
+    print
+    print "The frame range of this project is: " + str(int(nuke.root()["first_frame"].value())) + " - " + str(int(nuke.root()["last_frame"].value()))
+    print "Input rendering frame range, start-end"
+    print
+    in_frame_range = raw_input()
+
+
+
+    cmd = "C:\\Program Files\\Nuke11.2v3\\Nuke11.2.exe --nukex -i -X " + in_write_name + " -F " + in_frame_range + " " + nk_file
+
+    subprocess.call(cmd)
+
+
+Render_Write("C:\\Users\\Alienware\\Desktop\\aaa_V001.nk")
 
 
 
