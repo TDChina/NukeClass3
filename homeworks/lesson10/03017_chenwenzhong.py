156156535ab6610becbd281636f74bd64c26148b
Wendell Chen <413739534@qq.com>
2020-04-12 13:14:50 +0800

    lesson10_v02

--- homeworks/lesson10/03017_chenwenzhong.py
+++ homeworks/lesson10/03017_chenwenzhong.py
@@ -1,4 +1,5 @@
 import nuke
+nuke.scriptSource('E:/Nuke_mylself/Nuke_Dev_test/test_01.nk')
 buttom_node = None
 for node in nuke.allNodes():
     if node.Class() != 'Viewer':
@@ -12,6 +13,7 @@ new_node = nuke.createNode('Merge')
 new_node.setXYpos(buttom_node.xpos()-new_node.screenWidth()/2+buttom_node.screenWidth()/2, buttom_node.ypos() + 50 )
 print nuke.allNodes()
 nuke.scriptSource('E:/Nuke_mylself/Nuke_Dev_test/test_02.nk')
+print nuke.allNodes()
 new_node.setInput(0, nuke.toNode('Dot38'))
 nuke.scriptSource('E:/Nuke_mylself/Nuke_Dev_test/test_03.nk')
 nuke.toNode('Dot8').setInput(0, nuke.toNode('Dot38'))
