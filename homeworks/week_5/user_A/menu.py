5e36d4f031f6431a26c711981127431ef8d50954
03009_ZhangYing <beimingxinglian@gmail.com>
2020-04-19 18:10:13 +0800

    修改语法错误

--- homeworks/week_5/user_A/menu.py
+++ homeworks/week_5/user_A/menu.py
@@ -2,9 +2,7 @@ import nuke
 if len(nuke.pluginPath(0)) == len(nuke.pluginPath(1)):
     print u'plugins all load'
 
-for i in range(1,4)
+for i in range(1, 4):
     command = 'lambda: nuke.message("Command{}")'.format(i)
     nuke.menu('Node Graph').addCommand('Command{}'.format(i), eval(command))
 
-
-print u'it`s menu.py'
--- homeworks/week_5/user_A_nuke.bat
+++ homeworks/week_5/user_A_nuke.bat
@@ -1,3 +1,3 @@
-set NUKE_PATH=G:\TDClass3\NukeClass3\nuke_lunch\user_A
+set NUKE_PATH=G:\TDClass3\NukeClass3\homeworks\week_5\user_A
 
 "C:\Program Files\Nuke11.1v1\Nuke11.1.exe"
